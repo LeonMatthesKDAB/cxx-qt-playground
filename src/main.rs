0eb7c23221650286bec6179414b3e04ec9d1968a
Leon Matthes <leon.matthes@kdab.com>
2023-05-29 13:11:32 +0200

    evdev tests

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
-// where cxx_qt::QObject is a trait that links T to qobject::T
-trait Constructor<T> {
-    fn construct(args: T) -> Self;
+use evdev::Device;
+use signal_hook::consts::TERM_SIGNALS;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+
+fn process_events(device: &mut Device) -> std::io::Result<()> {
+    let events = device.fetch_events()?;
+    for ev in events {
+        println!("{:?}", ev);
+    }
+    Ok(())
 }
-struct MyStruct {
-    x: i32,
-    y: String,
-}
-impl Constructor<(i32, String)> for MyStruct {
-    fn construct((num, string): (i32, String)) -> Self {
-        MyStruct { x: num, y: string }
+
+fn grab_inputs(device: &mut Device) {
+    let terminate = Arc::new(AtomicBool::new(false));
+    for sig in TERM_SIGNALS {
+        signal_hook::flag::register(*sig, Arc::clone(&terminate)).unwrap();
+    }
+
+    let mut device = Device::open("/dev/input/event4").unwrap();
+
+    std::thread::spawn(move || {
+        device.grab().unwrap();
+        loop {
+            if let Err(e) = process_events(&mut device) {
+                eprintln!("Error: {}", e);
+            }
+        }
+    });
+
+    while !terminate.load(Ordering::Relaxed) {
+        std::thread::sleep(std::time::Duration::from_millis(100))
     }
 }
 
 fn main() {
-    let my = <MyStruct as Constructor<(i32, String)>>::construct((5, "hello".to_owned()));
-    println!("MyStruct: {}, {}", my.x, my.y);
+    for (path, device) in evdev::enumerate() {
+        println!(
+            "{}, {}",
+            path.to_string_lossy(),
+            device.name().unwrap_or("Unknown Device")
+        );
+    }
 }
